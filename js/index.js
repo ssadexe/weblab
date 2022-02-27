0f745fbdc3be62a9393e8159efa0cb0616f13f2d
ssadexe <diomin.vd13@icloud.com>
2022-02-27 21:17:26 +0300

    redisign main page

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
 function sidebar() {
     let sidebar = document.getElementById('sidebar')
     let sidebarBtn = document.getElementById('sidebar-btn');
-    let wrapper = document.getElementById('wrapper')
 
     sidebarBtn.classList.toggle('sidebar-btn-animation'); 
     sidebar.classList.toggle('sidebar-animation');
-    wrapper.classList.toggle('wrapper-animation');
 
     if (sidebar.classList.contains('sidebar-animation')) {
         document.body.style.position = 'fixed';
     } else {
         document.body.style.position = 'relative';
     }
-    
-    document.getElementById('bottom-wrapper').style.height = '100vh';
+
+    let sidebarBtnIcon = document.getElementById('sidebar-btn-icon');
+    if (sidebar.classList.contains('sidebar-animation')) {
+        sidebarBtnIcon.src = 'img/icons/UI/cancel.svg';
+    } else {
+        sidebarBtnIcon.src = 'img/icons/UI/menu.svg';
+    }
 }
 
-let sidebarMenu = document.getElementById('sidebar-menu');sidebarMenu.onclick = function closeSidebar() {
-    sidebar()
+let sidebarMenu = document.getElementById('sidebar-menu');
+sidebarMenu.onclick = function closeSidebar() {
+    sidebar();
+    
 }
 
 let backBtn = document.getElementById('backBtn');
