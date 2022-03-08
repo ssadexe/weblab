f0a9a405d1dd86203cd1f308010ddd9fdf308190
ssadexe <diomin.vd13@icloud.com>
2022-03-08 22:59:44 +0300

    new project

--- js/index.js
+++ js/index.js
@@ -1,31 +1,14 @@
 function sidebar() {
-    let sidebar = document.getElementById('sidebar')
-    let sidebarBtn = document.getElementById('sidebar-btn');
+    let sidebarBtn = document.querySelector('.sidebar-btn')
+    let sidebar = document.querySelector('.sidebar')
+    sidebar.classList.toggle('sidebar-animation')
+    sidebarBtn.classList.toggle('sidebar-btn-animation')
 
-    sidebarBtn.classList.toggle('sidebar-btn-animation'); 
-    sidebar.classList.toggle('sidebar-animation');
-
-    if (sidebar.classList.contains('sidebar-animation')) {
-        document.body.style.position = 'fixed';
-    } else {
-        document.body.style.position = 'relative';
-    }
-
-    let sidebarBtnIcon = document.getElementById('sidebar-btn-icon');
-    if (sidebar.classList.contains('sidebar-animation')) {
-        sidebarBtnIcon.src = 'img/icons/UI/cancel.svg';
-    } else {
-        sidebarBtnIcon.src = 'img/icons/UI/menu.svg';
-    }
-}
-
-let sidebarMenu = document.getElementById('sidebar-menu');
-sidebarMenu.onclick = function closeSidebar() {
-    sidebar();
+    let sidebarBtnIcon = document.getElementById('sidebar-btn__icon')
     
-}
-
-let backBtn = document.getElementById('backBtn');
-backBtn.onclick = function backPage() {
-    history.back();
+    if (sidebar.classList.contains('sidebar-animation')) {
+        sidebarBtnIcon.src = 'img/icons/UI/cancel.svg'
+    } else {
+        sidebarBtnIcon.src = 'img/icons/UI/menu.svg'
+    }
 }
